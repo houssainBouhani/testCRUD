07274bd489270b9ddd7f0fc89734ab664d720664
houssain bouhani <houssain.bouhani@gmail.com>
2022-02-28 14:26:32 +0100

    initial commit

--- src/axiosConfig/index.js
+++ src/axiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "https://goofy-euclid-b2bf86.netlify.app",
+  baseURL: "http://localhost:3001",
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
