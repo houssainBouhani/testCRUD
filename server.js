64b70bb064a1f4922eff50f1d0154ba4258d7884
houssain bouhani <houssain.bouhani@gmail.com>
2022-02-28 14:11:24 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const data = require("./db.json");
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(data);
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
